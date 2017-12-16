3b7b0935e5ac223b3d3cb16251afbf24afb44a7d
Matthew Hu <matthu24@gmail.com>
2017-12-15 21:01:09 -0800

    Fix homescreen API call

--- components/home_screen/home_screen_container.js
+++ components/home_screen/home_screen_container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { values } from 'lodash';
-import { fetchProjects } from '../../actions/project_actions';
+import { fetchSingleUser } from '../../actions/user_actions';
 import HomeScreen from './home_screen';
 
 const mapStateToProps = state => ({
@@ -37,7 +37,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchProjects: (userId) => dispatch(fetchProjects(userId))
+  fetchSingleUser: (userId) => dispatch(fetchSingleUser(userId))
 });
 
 export default connect(
